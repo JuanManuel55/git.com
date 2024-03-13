ac4e3854b14b01f2086425b09c2cd8e5bc16259a
JuanManuel55 <160889261+JuanManuel55@users.noreply.github.com>
2024-03-13 13:59:24 -0400

    Add files via upload

---
+++ tarea3.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1><center>Hello Word.com</center><h1>"
+
+@app.route('/calculadora/<operation>/<int:num1>/<int:num2>')
+def calculadora(n1,n2,operation):
+    if operation == "+":
+        return f" La suma de como resultado: {n1 + n2}"
+    elif operation == "-":
+        return f" La resta da como resultado: {n1 - n2}"
+    elif operation == "*":
+        return f" La multiplicacion da como resultado: {n1 * n2}"
+    elif operation == "/":
+        return f" La divicion de como resultado: {n1 / n2}"
+    elif operation == "**":
+        return f" La elevacion da como resultado: {n1 ** n2}"
+    
+
+if __name__ == '__main__':
+    app.run(port=5500 , debug=True)
